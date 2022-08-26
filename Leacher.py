0e505538336b55fc215706a2d319a531bbb4d172
Sardar <88272644+Sardarcybery@users.noreply.github.com>
2022-08-26 13:28:04 +0430

    Update Leacher.py

--- Leacher.py
+++ Leacher.py
@@ -11,6 +11,7 @@ class Leacher:
     def FollowersLeacher(self):
         try:
             app.login(self.Username,self.Passsord)
+            print(Fore.LIGHTGREEN_EX + "Login . . .")
             FollowerList = []
             Profile = instaloader.Profile.from_username(app.context,self.Target)
             SleepTime = 0
@@ -32,31 +33,5 @@ class Leacher:
                     sleep(3600)
                     AntiBan = 0
             FileFollowers.close()
-        except Exception as ex:
-            print(str(ex))
-    def Followingleacher(self):
-        try:
-            app.login(self.Username,self.Passsord)
-            FollowingList = []
-            Profile = instaloader.Profile.from_username(app.context,self.Target)
-            SleepTime = 0
-            AntiBan = 0
-            count = 0
-            FileFollowing = open("Following.txt", "a+")
-            for Followers in Profile.get_followees:
-                print(Followers.username)
-                FollowingList.append(Followers.username)
-                FileFollowing.write(f'{FollowingList[count]}\n') 
-                count += 1
-                SleepTime += 1
-                AntiBan += 1
-                if SleepTime == 100:
-                    sleep(30)
-                    SleepTime = 0
-                if AntiBan == 8000:
-                    print(Fore.LIGHTRED_EX + "Sleep 3600 Second(1 Hours) , Do not exit the App")
-                    sleep(3600)
-                    AntiBan = 0
-                FileFollowing.close()
         except Exception as ex:
             print(str(ex))
