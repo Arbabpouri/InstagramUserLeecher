0a5deed57fe4928a25bb23d439349591b956b302
Sardar <88272644+Sardarcybery@users.noreply.github.com>
2022-08-26 15:25:07 +0430

    Update Leacher.py

--- Leacher.py
+++ Leacher.py
@@ -1,44 +1,37 @@
 #in the name of god | Coded By SardarCyberym | Yarnovin | GitHub.com/SardarCybery
-from Leacher.Leacher import Leacher
+import instaloader
+from time import sleep
 from colorama import Fore
-from re import match
-from os import system,name
-system('clear' if name == 'posix' else 'cls')
-
-def GiveUserName(): #Get Username
-    global Username
-    Username = input(Fore.GREEN + "Username : ")
-    CheckUsername = match(r"^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$",Username)
-    if CheckUsername:
-        GivePassword()
-    else:
-        print(Fore.RED + "Incorrect Value")
-        GiveUserName()
-
-def GivePassword(): #Get Password
-    global Password
-    Password = input(Fore.GREEN + "Password : ")
-    if int(len(Password)) < 8 :
-        print(Fore.RED + "Incorrect Value")
-        GivePassword()
-    else:
-        GiveTarget()
-
-def GiveTarget(): #Get Target
-    global Target
-    Target = input(Fore.GREEN + "Target Username : ")
-    CheckTarget = match(r"^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$",Target)
-    if CheckTarget:
-        Leacher(Username,Password,Target).FollowersLeacher()
-    else:
-        print(Fore.RED + "Incorrect Value")
-        GiveTarget()
-
-
-if __name__ == '__main__' :
-    print(f"""
-{Fore.LIGHTBLUE_EX}Telegram : {Fore.LIGHTGREEN_EX}t.me/Mr_Nazism
-{Fore.LIGHTCYAN_EX }Github : {Fore.LIGHTYELLOW_EX}Github.com/SardarCybery
-{Fore.LIGHTRED_EX}WebSite : {Fore.LIGHTMAGENTA_EX}YarNovin.ir
-    """)
-    GiveUserName()
+app = instaloader.Instaloader()
+class Leacher:
+    def __init__(self,Username:str,Password:str,Target:str) -> None:
+        self.Username = Username
+        self.Passsord = Password
+        self.Target = Target
+    def FollowersLeacher(self):
+        try:
+            app.login(self.Username,self.Passsord)
+            print(Fore.LIGHTGREEN_EX + "Login . . .")
+            FollowerList = []
+            Profile = instaloader.Profile.from_username(app.context,self.Target)
+            SleepTime = 0
+            AntiBan = 0
+            count = 0
+            FileFollowers = open("Followers.txt", "a+")
+            for Followers in Profile.get_followers():
+                print(Followers.username)
+                FollowerList.append(Followers.username)  
+                FileFollowers.write(f'{FollowerList[count]}\n')
+                count += 1
+                SleepTime += 1
+                AntiBan += 1
+                if SleepTime == 100:
+                    sleep(30)
+                    SleepTime = 0
+                if AntiBan == 8000:
+                    print(Fore.LIGHTRED_EX + "Sleep 3600 Second(1 Hours) , Do not exit the App")
+                    sleep(3600)
+                    AntiBan = 0
+            FileFollowers.close()
+        except Exception as ex:
+            print(str(ex))
